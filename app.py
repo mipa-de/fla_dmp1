e24736861a33694fb4f0d8df06c1225290e41923
mipa-de <rootxy@live.de>
2018-11-25 21:59:50 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask_ask import Ask, statement, question
+import yaml
+import sys
+from random import randint
+
+
+def yml_loader(file):
+    try:
+        with open(file, 'r') as stream:
+            return yaml.load(stream)
+    except IOError:
+        sys.exit(1)
+
+
+template = yml_loader('template.yml')
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+
+@app.route('/')
+def homepage():
+    homepage = template['start_msg']
+    return homepage
+
+
+@ask.launch
+def start_skill():
+    welcome_msg = template['welcome_msg']
+    return question(welcome_msg)
+
+@ask.intent('AMAZON.NoIntent')
+def no_intent():
+    bye_text = template['bye_msg']
+    return statement(bye_text)
+
+
+@ask.intent('AMAZON.StopIntent')
+def stop_intent():
+    bye_text = template['bye_msg']
+    return statement(bye_text)
+
+
+@ask.intent('AMAZON.CancelIntent')
+def cancel_intent():
+    bye_text = template['bye_msg']
+    return statement(bye_text)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
